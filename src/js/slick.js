c98d749fbb38e0a89ecfe9373120a9de61e1a6aa
ForestDr <nikita2001395@gmail.com>
2023-01-29 13:39:35 +0200

    add fix slider and section

--- src/js/slick.js
+++ src/js/slick.js
@@ -35,7 +35,7 @@ $('.slick-small').slick({
     '<button type="button" class="slick-small-prev"><svg class="slick-arrow slick-small-prev-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 15.49h-26.16l6.019-6.061-1.419-1.429-8.44 8.5 8.44 8.5 1.419-1.429-6.019-6.061h26.16v-2.021z"></path></svg></button>',
   nextArrow:
     '<button type="button" class="slick-small-next"><svg class="slick-arrow slick-small-next-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.559 8l-1.419 1.429 6.019 6.061h-26.16v2.021h26.159l-6.018 6.061 1.419 1.429 8.441-8.5-8.441-8.5z"></path></svg></button>',
-  infinite: false,
+  infinite: true,
   speed: 300,
   slidesToScroll: 1,
   adaptiveHeight: true,
