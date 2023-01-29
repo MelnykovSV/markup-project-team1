c8c067168a21de25b19f03910e9d482e040c2eeb
MelnykovSV <svmelnykov@gmail.com>
2023-01-29 04:30:53 +0200

    Update slick.js

--- src/js/slick.js
+++ src/js/slick.js
@@ -4,7 +4,7 @@ $('.slick-header').slick({
     '<button type="button" class="slick-header-prev"><svg class="slick-arrow slick-header-prev-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 15.49h-26.16l6.019-6.061-1.419-1.429-8.44 8.5 8.44 8.5 1.419-1.429-6.019-6.061h26.16v-2.021z"></path></svg></button>',
   nextArrow:
     '<button type="button" class="slick-header-next"><svg class="slick-arrow slick-header-next-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.559 8l-1.419 1.429 6.019 6.061h-26.16v2.021h26.159l-6.018 6.061 1.419 1.429 8.441-8.5-8.441-8.5z"></path></svg></button>',
-  infinite: true,
+  infinite: false,
   speed: 300,
   slidesToScroll: 1,
   adaptiveHeight: true,
