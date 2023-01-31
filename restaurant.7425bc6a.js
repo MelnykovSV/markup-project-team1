14c016cd36a787f0d6d7e5b66d753e9304b9dc9e
MelnykovSV <114297826+MelnykovSV@users.noreply.github.com>
2023-01-31 02:52:05 +0000

    Deploying to gh-pages from @ MelnykovSV/markup-project-team1@57abd0e573cdc9256d3920b63f615544285174eb 🚀

---
+++ restaurant.7425bc6a.js
@@ -0,0 +1,2 @@
+Modernizr.on("webp",(function(o){const c=document.querySelectorAll(".js-bg");o?c.forEach((o=>{o.classList.add("webp")})):c.forEach((o=>{o.classList.add("no-webp")}))}));
+//# sourceMappingURL=restaurant.7425bc6a.js.map
---
+++ restaurant.d1d3c941.js
@@ -0,0 +1,2 @@
+Modernizr.on("webp",(function(n){var o=document.querySelectorAll(".js-bg");n?o.forEach((function(n){n.classList.add("webp")})):o.forEach((function(n){n.classList.add("no-webp")}))}));
+//# sourceMappingURL=restaurant.d1d3c941.js.map
