f4dc1078a84884aa7a23c6775d60faf435c60137
ForestDr <nikita2001395@gmail.com>
2023-01-31 11:39:19 +0200

    add slider into hotel details

--- src/js/slick.js
+++ src/js/slick.js
@@ -61,6 +61,31 @@ $('.slick-details').slick({
   cssEase: 'linear',
 });
 
+$('.slick-details-secondary').slick({
+  dots: false,
+  prevArrow:
+    '<button type="button" class="slick-details-prev"><svg class="details-slick-arrow slick-details-prev-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M31 15.49h-26.16l6.019-6.061-1.419-1.429-8.44 8.5 8.44 8.5 1.419-1.429-6.019-6.061h26.16v-2.021z"></path></svg></button>',
+  nextArrow:
+    '<button type="button" class="slick-details-next"><svg class="details-slick-arrow slick-details-next-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.559 8l-1.419 1.429 6.019 6.061h-26.16v2.021h26.159l-6.018 6.061 1.419 1.429 8.441-8.5-8.441-8.5z"></path></svg></button>',
+  infinite: true,
+  speed: 300,
+  slidesToScroll: 1,
+  adaptiveHeight: true,
+  slidesToShow: 2,
+  // centerMode: true,
+  variableWidth: true,
+  cssEase: 'linear',
+
+  responsive: [
+    {
+      breakpoint: 1023,
+      settings: {
+        initialSlide: 1,
+      },
+    },
+  ],
+});
+
 // $('.responsive').slick({
 //     // dots: true,
 //     prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
