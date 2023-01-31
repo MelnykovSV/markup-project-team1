545962613d56dc86d970e2cf5467d5d6b3235bd6
MelnykovSV <svmelnykov@gmail.com>
2023-01-31 15:28:55 +0200

    bg-webp

--- src/js/webp-detect.js
+++ src/js/webp-detect.js
@@ -2,9 +2,9 @@ Modernizr.on('webp', function (result) {
   const bgs = document.querySelectorAll('.js-bg');
 
   if (result) {
-    bgs.forEach(ele => {
-      ele.classList.add('webp');
-    });
+    // bgs.forEach(ele => {
+    //   ele.classList.add('webp');
+    // });
   } else {
     bgs.forEach(ele => {
       ele.classList.add('no-webp');
